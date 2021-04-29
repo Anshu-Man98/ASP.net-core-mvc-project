bafa664a4d5d4093d0bd2827e1daa4e52f738d53
Amnshuman <75073445+Anshu-Man98@users.noreply.github.com>
2021-04-29 09:08:16 +0530

    Add files via upload

---
+++ Context/userContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using DeactivationForm.Models;
+
+namespace DeactivationForm.Context
+{
+    public class userContext:DbContext
+    {
+        public DbSet<user> users { get; set; }
+    }
+}
---
+++ Models/user.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DeactivationForm.Models
+{
+    public class user
+
+    {
+        
+        public string Fname { get; set; }
+        [Required]
+        [StringLength(20,ErrorMessage ="Name not be exceed")]
+        public string Lname { get; set; }
+
+        [Required]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "please enter valid email")]
+        public string Email { get; set; }
+        [Key]
+        public string gId { get; set; }
+        [Required (ErrorMessage ="last working date is required")]
+        public DateTime date { get; set; }
+    }
+}
+
