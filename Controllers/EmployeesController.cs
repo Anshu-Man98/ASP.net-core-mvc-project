a68b5bf6fc7e6a05b6b94c0f3d1faf503490e933
Amnshuman <75073445+Anshu-Man98@users.noreply.github.com>
2021-04-30 15:47:26 +0530

    Add files via upload

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmployeeDeactivation.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeDeactivation.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeDeactivationContext _context;
+
+        public EmployeesController(EmployeeDeactivationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employee.ToListAsync());
+        }
+
+       
+
+            // GET: Employees/Details/5
+            public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee
+                .FirstOrDefaultAsync(m => m.gId == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Firstname,Lastname,Email,gId,date")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+    }
+}
