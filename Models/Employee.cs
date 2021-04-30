5603b83ccb39371e7c39b151687fbacebd4ea69c
Amnshuman <75073445+Anshu-Man98@users.noreply.github.com>
2021-04-30 15:47:56 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using EmployeeDeactivation.Controllers;
+
+namespace EmployeeDeactivation.Models
+{
+    public class Employee
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "FirstName sholud not excede be more than 30 letters")]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "FirstName sholud not contain Special characters")]
+        public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "LastName sholud not excede be more than 30 letters")]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "LastName sholud not contain Special characters")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage ="email address is Required")]
+        [MaxLength(50)]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "please enter valid email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Key]
+        [Required(ErrorMessage = "GID is Required")]
+        public string gId { get; set; }
+
+        
+        [FutureDate(ErrorMessage ="Date should be in the future")]
+        public DateTime date { get; set; }
+    }
+   
+    
+}
