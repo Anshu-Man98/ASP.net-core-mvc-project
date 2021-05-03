3628e0ab2af087d116ab2ae982f6f167e410bc62
Amnshuman <75073445+Anshu-Man98@users.noreply.github.com>
2021-05-03 16:01:11 +0530

    Add files via upload

---
+++ EmployeeDeactivation/Models/FutureDateAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EmployeeDeactivation.Models;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EmployeeDeactivation.Models
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return value != null && (DateTime)value > DateTime.Now;
+        }
+    }
+}
---
+++ EmployeeDeactivation/Models/Teams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeDeactivation.Models
+{
+    public class Teams
+    {
+      
+
+        public string TeamName { get; set; }
+       
+        [Key]
+        public string SponsorGID { get; set; }
+       
+       
+    }
+
+    
+}
