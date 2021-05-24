185ac03a7371c0079be85d6b76883778fd97b252
Amnshuman <75073445+Anshu-Man98@users.noreply.github.com>
2021-05-24 08:37:36 +0530

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeDeactivation.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeDeactivation.Controllers
+{
+    [AllowAnonymous]
+    public class AdminController : Controller 
+    {
+        private readonly IAdminDataOperation _adminDataOperation;
+        public AdminController(IAdminDataOperation adminDataOperation)
+        {
+            _adminDataOperation = adminDataOperation;
+        }
+
+        public IActionResult AdminPage()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Admin/SponsorDetails")]
+        public JsonResult SponsorDetails()
+        {
+            return Json(_adminDataOperation.RetrieveSponsorDetails());
+        }
+
+        [HttpPost]
+        [Route("Admin/AddSponsorDetails")]
+        public JsonResult AddSponsorDetails(string teamName, string sponsorFirstName, string sponsorLastName, string sGid, string sponsorEmail, string sDepartment, string rmEmail)
+        {
+            var updateStatus = _adminDataOperation.AddSponsorData(teamName, sponsorFirstName, sponsorLastName, sGid, sponsorEmail, sDepartment, rmEmail);
+
+            return Json(true);
+        }
+    }
+}
---
+++ Interface/IAdminDataOperation.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeDeactivation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeDeactivation.Interface
+{
+   public interface IAdminDataOperation
+    {
+        List<Teams> RetrieveSponsorDetails();
+        Task<bool> AddSponsorData(string teamName, string sponsorFirstName, string sponsorLastName, string sGid, string sponsorEmail, string sDepartment, string rmEmail);
+    }
+}
